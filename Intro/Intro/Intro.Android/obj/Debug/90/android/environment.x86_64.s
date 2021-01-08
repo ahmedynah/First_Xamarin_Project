46453c036573b26e492604cc858573563de65ae8
Ahmed Abouelnasr <ahmedhanyhasn@yahoo.com>
2021-01-08 20:40:43 +0200

    for submission

--- Intro/Intro/Intro.Android/obj/Debug/90/android/environment.x86_64.s
+++ Intro/Intro/Intro.Android/obj/Debug/90/android/environment.x86_64.s
@@ -72,7 +72,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"f02d005f-3b91-4659-887a-ea0387d84d2f"
+	.asciz	"393189ed-48c5-4e6a-8337-9d86adc53abf"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
